d77cb885f6c5ccc32f82171e2d7d17bd6efd280b
Aiia22 <64868714+Aiia22@users.noreply.github.com>
2022-06-28 15:22:19 +0100

    Initial commit

---
+++ Entities/EntityBase.cs
@@ -0,0 +1,11 @@
+﻿namespace WiredBrainCoffee.StorageApp.Entities
+{
+
+    public class EntityBase : IEntity
+    {
+        public int Id { get; set; }
+    }
+}
+
+
+
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using WiredBrainCoffee.StorageApp.Entities;
+using WiredBrainCoffee.StorageApp.Repositories;
+
+
+
+
+
+
+namespace WiredBrainCoffee.StorageApp
+{
+  class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var employeesRepository = new GenericRepository<Employee>();
+            AddEmployees(employeesRepository);
+            GetEmployeeById(employeesRepository);
+
+            var organizationRepository = new GenericRepository<Organization>();
+            AddOrganization(organizationRepository);
+
+            Console.ReadLine();
+        }
+
+        private static void GetEmployeeById(GenericRepository<Employee> employeesRepository)
+        {
+            var employee = employeesRepository.GetById(2);
+            Console.WriteLine($"Employee with Id 2: {employee.FirstName}");
+        }
+
+        private static void AddEmployees(GenericRepository<Employee> employeesRepository)
+        {
+            employeesRepository.Add(new Employee { FirstName = "Julia" });
+            employeesRepository.Add(new Employee { FirstName = "Anna" });
+            employeesRepository.Add(new Employee { FirstName = "Thomas" });
+            employeesRepository.Save();
+        }
+
+        private static void AddOrganization(GenericRepository<Organization> organizationRepository)
+        {
+            organizationRepository.Add(new Organization { Name = "Plurialsight" });
+            organizationRepository.Add(new Organization { Name = "Globomantic" });
+            organizationRepository.Save();
+        }
+    }
+}
+
